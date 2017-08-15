13033d750b6e6b3efcb4192cea1af7f7db09ad63
Theng Hao Png <jacpng@gmail.com>
2017-08-16 00:47:59 +0800

    todolist_draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ItemListPage } from '../pages/item-list/item-list';
 
 @NgModule({
   declarations: [
